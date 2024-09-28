c60b18ca0465060132bfc4bfe163678dc2051622
zeyadcoder <Zeyad.bawany@gmail.com>
2024-09-28 10:52:42 +0500

    work6

---
+++ Lesson6.py
@@ -0,0 +1,27 @@
+class student:
+    grade = 2
+    print(grade)
+ob = student()
+
+#activity 2
+
+class vehicle: 
+    def __init__ (self, max_speed, milage2):
+        self.max_speed = max_speed
+        self.milage2 = milage2
+ob = vehicle(160, 18)
+print (ob. max_speed)
+print (ob.milage2)
+
+
+
+
+    
+    
+        
+
+        
+        
+    
+
+ 
